def1d70cb28cf10c66253f2eb70a2573be33c2e8
Emilio <emiliomoreno03096@gmail.com>
2024-12-05 16:37:23 -0600

    added Vision

--- src/main/java/frc/robot/Subsystems/RobotPosition.java
+++ src/main/java/frc/robot/Subsystems/RobotPosition.java
@@ -1,6 +1,10 @@
 package frc.robot.Subsystems;
 
+import java.util.Optional;
+
 import frc.robot.MathUtils.Vector2;
+import frc.robot.Subsystems.Sensors.Pigeon;
+import frc.robot.Subsystems.Sensors.Vision;
 
 /**
  * A class to handle the robot position, tracking the robot's position on the field 
@@ -8,17 +12,26 @@ import frc.robot.MathUtils.Vector2;
  */
 public class RobotPosition {
     private Vector2 position;
+    private double VISION_CORRECTION_FACTOR = .7;
 
     public void init(){
-
+        position = new Vector2();
     }
 
     public void update(){
+        position.add(Odometry.getPositionChange());
+        
+        Optional<Vector2> visionPosition = Vision.getPosition();
 
+        if(visionPosition.isPresent()){
+            Vector2 positionError = visionPosition.get().sub(position);
+            position = position.add(positionError.mul(VISION_CORRECTION_FACTOR));
+        }
     }
 
     public Vector2 getPostion(){
         return position;
+        
     }
 
 }
